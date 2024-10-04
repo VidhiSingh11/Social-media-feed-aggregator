ece507061bdf8538c6320a53d455210cdba0aa7e
VidhiSingh11 <vidhisingh112003@gmail.com>
2024-10-04 09:43:12 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaFeedAggregator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/scripts.js
@@ -0,0 +1,2 @@
+// JavaScript functionality can be added here
+console.log("Social Media Feed Aggregator loaded.");
